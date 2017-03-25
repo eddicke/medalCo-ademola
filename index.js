bcddf752f133fe357dd260a6542c09ad9cb6ce44
eddicke <adenekanademola931@gmail.com>
2017-03-24 21:04:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,51 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+    console.log('i think i saw something...');
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+ console.log('i think i saw something...');
+  });
+  //game try!
+  socket.on('ademola', function(gme){
+    io.emit('ademola', gme);
+  });
+  //power up
+   socket.on('power', function(hunts){
+    io.emit('power', hunts);
+  });
+  //webcam
+socket.on('webcam', function(webcam){
+    io.emit('webcam', webcam);
+  });
+  //move down
+  socket.on('player2-1', function(down){
+    io.emit('player2-1', down);
+  });
+  //player2
+  socket.on('player2', function(hunts){
+    io.emit('player2', hunts);
+  });
+  //move down
+  socket.on('player2-1', function(down){
+    io.emit('player2-1', down);
+  });
+  //player1
+   socket.on('hunts', function(hunts){
+    io.emit('hunts', hunts);
+  });
+   socket.on('player1', function(hunts){
+    io.emit('player1', hunts);
+  });
+  //state
+  socket.on('state', function(stats){
+    io.emit('state', stats);
+  });
+  //move down
+  socket.on('player1-1', function(down){
+    io.emit('player1-1', down);
   });
 });
-
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
